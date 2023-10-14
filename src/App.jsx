a73c2a13996f0b29aaf62db6ca61bab3515b6303
Radja Thaher <radjapolem@gmail.com>
2023-10-14 18:31:00 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styles from './style.js'
+
+const App = () => (
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX}`}>
+
+      </div>
+    </div>
+)
+
+
+export default App
