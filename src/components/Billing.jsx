491478a03bc5a551edd4c9343b099af6f76d535d
Radja Thaher <radjapolem@gmail.com>
2023-11-09 18:02:04 -0500

    commit

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { apple, bill, google } from '../assets'
+import styles, { layout } from '../style'
 
 const Billing = () => {
   return (
-    <div>Billing</div>
+    <section>
+      <div>
+
+      </div>
+
+      <div>
+        
+      </div>
+    </section>
   )
 }
 
